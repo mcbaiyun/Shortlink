07dd391dd5e416e50b4f772ba4f31cef923220eb
mcbaiyun <61493882+mcbaiyun@users.noreply.github.com>
2021-05-02 20:58:22 +0800

    更新 api.php

--- api/api.php
+++ api/api.php
@@ -1,4 +1,18 @@
 <?php
+    function get_http_code($url) { 
+        $curl = curl_init(); 
+        curl_setopt($curl, CURLOPT_URL, $url); //设置URL 
+        curl_setopt($curl, CURLOPT_HEADER, 1); //获取Header 
+        curl_setopt($curl, CURLOPT_NOBODY, true); //只保留Head 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //数据到成字符串
+        $data = curl_exec($curl); //开始请求 
+        $return = curl_getinfo($curl, CURLINFO_HTTP_CODE); //获取状态码
+           
+        curl_close($curl); //关闭请求 
+           
+        return $return; 
+    }
+
     // 引入类
     require_once('../inc/require.php');
     global $config;
@@ -13,6 +27,12 @@
             if(!strstr($_GET['url'], 'http://') && !strstr($_GET['url'], 'https:')) $_GET['url'] = 'http://' . $_GET['url'];
             // 检测网址格式是否正确
             $is_link = preg_match('(http(|s)://([\w-]+\.)+[\w-]+(/)?)', $_GET['url']);
+            //判断该网页是否能正常访问 -- 白云
+        $webcode = number_format(get_http_code($_GET['url']) or die('{"success":"false","content":"\u8be5\u94fe\u63a5\u65e0\u6cd5\u6b63\u5e38\u8bbf\u95ee\uff0c\u8bf7\u68c0\u67e5\u662f\u5426\u6b63\u786e\uff01"}');
+            if($webcode <200 or $webcode>=500){
+                $opt['content'] = '该链接无法正常访问，请确认输入内容是否正确。';
+                die('{"success":"false","content":"\u8be5\u94fe\u63a5\u65e0\u6cd5\u6b63\u5e38\u8bbf\u95ee\uff0c\u8bf7\u68c0\u67e5\u662f\u5426\u6b63\u786e\uff01"}');
+            }
             // 判断条件
             if($_GET['url'] != '' && !strstr($_GET['url'], $_SERVER['HTTP_HOST']) && $is_link) {
                 $opt['success'] = 'true';
